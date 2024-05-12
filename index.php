adc8c044087d06c56aa6a4b744ba14e4f56d8d5b
Rubbershredder <145557629+Rubbershredder@users.noreply.github.com>
2024-05-12 20:05:01 +0530

    addition of marquee

--- index.php
+++ index.php
@@ -129,7 +129,43 @@
 
             </div><!-- END Reveal panel section -->
         </section>
-        <div class="spacer"></div>
+        <div class="Accreditation-container">
+            <h1 class="AccreditationTitle">Accreditation & Rankings</h1>
+            <div class="Accreditation-slider">
+                <div class="Accreditation-slide-track">
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\aicte-logo.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\b-naac.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\karnataka-state-gvt.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\nba.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\vtu-logo.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\aicte-logo.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\b-naac.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\karnataka-state-gvt.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\nba.png" height="128" width="128" alt />
+                    </div>
+                    <div class="Accreditation-slide">
+                        <img src="assets\Approvals\vtu-logo.png" height="128" width="128" alt />
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="spacer"></div>
         <div class="Footer"><?php include "src/footer.php"; ?></div>
 </body>
