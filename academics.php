ee7062e114cf2b1219b14b7f04fa079142a75134
ins4ne <150160605+NikZone1@users.noreply.github.com>
2024-06-26 12:29:35 +0530

    acdemics-bg

--- academics.php
+++ academics.php
@@ -16,7 +16,7 @@
 <body>
     <div class="navBar"><?php include "src/navbar.php"; ?></div>
     <div class="aca-img">
-        <img class="aca-bgimg" src="assets/bgimages/112.jpg" alt="">
+        <img class="aca-bgimg" src="assets/bgimages/111.jpg" alt="">
     </div>
     <div class="aca-navbar">
         <ul id="aca-navLinks">
